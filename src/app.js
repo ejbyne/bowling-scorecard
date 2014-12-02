51c6db21ef9f1db7b1a61db094e3be7cff0be7c2
ed <ejbyne@gmail.com>
2014-12-02 13:21:56 +0000

    refactoring

--- src/app.js
+++ src/app.js
@@ -26,31 +26,23 @@ $(document).ready(function(){
     event.preventDefault()
     var rollEntry = parseInt($(this).data('pick'))
     var enterRoll = scorecard.enterRoll(rollEntry)
-
     if(enterRoll != "Incorrect number") {
-      if(parseInt(scorecard.currentFrame().rolls.length) === 1
-      && parseInt(scorecard.currentFrame().rolls[0]) < 10) {
+      if(parseInt(scorecard.currentFrame().rolls.length) === 1 && parseInt(scorecard.currentFrame().rolls[0]) < 10) {
         for (var i = 0; i <= 10; i++) {
           var unavailable = ('#' + String(i));
           if ((parseInt(rollEntry) + i) > 10) {
-            $(unavailable).unbind('mouseenter');
-            $(unavailable).unbind('mouseleave');
-            $(unavailable).css('opacity', 0.2);
+            $(unavailable).unbind('mouseenter').unbind('mouseleave').css('opacity', 0.2);
           }
         } 
       }
-      else {
-        $('#list li').css('opacity', 0.8);
-        $('#list li').mouseenter(function() {
-          $(this).css('opacity', 1) });
-        $('#list li').mouseleave(function() {
-          $(this).css('opacity', 0.8) });
+      else { $('#list li').css('opacity', 0.8).mouseenter(function() { $(this).css('opacity', 1) }).mouseleave(function() { $(this).css('opacity', 0.8) });
       }
-
     }
+    
     if(scorecard.isFinished()) {
       $('#pin_request').css("display", "none")
     }
+
     gameStatus.addText(gameStatus.printGameStatus(scorecard))
     tableData.addText(tableData.printData(scorecard))
     gameMessage.addText(gameMessage.printGameMessage(scorecard))
